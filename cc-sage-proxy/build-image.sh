eeb4b13d15f8bd8eef749aa80bf42ff6d32df772
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-03-12 15:29:52 -0500

    added extra file tree

--- cc-sage-proxy/build-image.sh
+++ cc-sage-proxy/build-image.sh
@@ -4,7 +4,7 @@
 openstack image save --file cc-sage-proxy.raw CC-Ubuntu18.04
 
 # spin up image in container and make modifications
-systemd-nspawn -i cc-sage-proxy.raw --bind-ro=/etc/resolv.conf bash -s <<EOF
+systemd-nspawn -i cc-sage-proxy.raw --bind-ro=/etc/resolv.conf --bind $PWD/extra:/extra bash -s <<EOF
 # install packages
 apt-get update && apt-get install -y git
 
@@ -13,6 +13,9 @@ echo '10.31.82.50 ep1' >> /etc/hosts
 echo '10.31.82.51 ep2' >> /etc/hosts
 echo '10.31.82.52 ep3' >> /etc/hosts
 echo '10.31.82.53 ep4' >> /etc/hosts
+
+# copy extra files
+cp -a /extra/* /
 EOF
 
 # create new image
