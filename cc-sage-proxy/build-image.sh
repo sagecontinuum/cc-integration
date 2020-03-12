d16d05c4c75ed6a7e5e917624d83c230c1b9dee2
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-03-12 16:33:48 -0500

    detect image format

--- cc-sage-proxy/build-image.sh
+++ cc-sage-proxy/build-image.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -ex
 
-# save ubuntu 18.04 base image
-openstack image save --file cc-sage-proxy.raw CC-Ubuntu18.04
+fatal() {
+    echo $*
+    exit 1   
+}
+
+# save base image
+imagename="CC-Ubuntu18.04"
+
+if openstack image show ${imagename} | grep -q 'disk_format.*qcow2'; then
+    openstack image save --file cc-sage-proxy.qcow2 ${imagename}
+    qemu-img convert cc-sage-proxy.qcow2 cc-sage-proxy.raw
+    rm cc-sage-proxy.qcow2
+elif openstack image show ${imagename} | grep -q 'disk_format.*raw'; then
+    openstack image save --file cc-sage-proxy.raw ${imagename}
+else
+    fatal "unknown image format"
+fi
 
 # spin up image in container and make modifications
 systemd-nspawn -i cc-sage-proxy.raw --bind-ro=/etc/resolv.conf --bind $PWD/extra:/extra bash -s <<EOF
