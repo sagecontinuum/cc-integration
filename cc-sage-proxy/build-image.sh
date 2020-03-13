aeb4fbc5baaa16efcf926dafaf1e28f6f0ec17f4
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-03-13 09:00:49 -0500

    refactored build steps

--- cc-sage-proxy/build-image.sh
+++ cc-sage-proxy/build-image.sh
@@ -5,21 +5,22 @@ fatal() {
     exit 1   
 }
 
-# save base image
-imagename="CC-Ubuntu18.04"
+download_base_image() {
+    imagename="CC-Ubuntu18.04"
 
-if openstack image show ${imagename} | grep -q 'disk_format.*qcow2'; then
-    openstack image save --file cc-sage-proxy.qcow2 ${imagename}
-    qemu-img convert cc-sage-proxy.qcow2 cc-sage-proxy.raw
-    rm cc-sage-proxy.qcow2
-elif openstack image show ${imagename} | grep -q 'disk_format.*raw'; then
-    openstack image save --file cc-sage-proxy.raw ${imagename}
-else
-    fatal "unknown image format"
-fi
+    if openstack image show ${imagename} | grep -q 'disk_format.*qcow2'; then
+        openstack image save --file cc-sage-proxy.qcow2 ${imagename}
+        qemu-img convert cc-sage-proxy.qcow2 cc-sage-proxy.raw
+        rm cc-sage-proxy.qcow2
+    elif openstack image show ${imagename} | grep -q 'disk_format.*raw'; then
+        openstack image save --file cc-sage-proxy.raw ${imagename}
+    else
+        fatal "unknown image format"
+    fi
+}
 
-# spin up image in container and make modifications
-systemd-nspawn -i cc-sage-proxy.raw --bind-ro=/etc/resolv.conf --bind $PWD/extra:/extra bash -s <<EOF
+setup_image() {
+    systemd-nspawn -i cc-sage-proxy.raw --bind-ro=/etc/resolv.conf --bind $PWD/extra:/extra bash -s <<EOF
 # install packages
 apt-get update && apt-get install -y git
 
@@ -28,6 +29,12 @@ cp -a /extra/* /
 
 systemctl enable setup-firewall.service
 EOF
+}
 
-# create new image
-openstack image create --public --disk-format raw --container-format bare --file cc-sage-proxy.raw CC-SAGE-Edge
+upload_image() {
+    openstack image create --public --disk-format raw --container-format bare --file cc-sage-proxy.raw CC-SAGE-Edge
+}
+
+download_base_image
+setup_image
+upload_image
